7da0fb58066f08849e3ee87ca017fe5d5d7902ca
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-09-03 21:36:13 -0500

    -Base project

---
+++ src/main/java/is2/Is2projectApplication.java
@@ -0,0 +1,12 @@
+package is2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is2projectApplication.class, args);
+	}
+}
