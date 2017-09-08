b5b70814f869b81576df16501dae6edbb875c50f
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-09-08 17:50:03 -0500

    -Added JSoup dependencies, and converted tabs to spaces

--- project/src/main/java/is2/Is2projectApplication.java
+++ project/src/main/java/is2/Is2projectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Is2projectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Is2projectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Is2projectApplication.class, args);
+    }
 }
