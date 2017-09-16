cd1a66fd02d5f766ab7455074b22b00ec321e9ca
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-09-16 00:45:12 -0500

    #2 Fix: problems with modules and persistent classes

--- project/src/main/java/is2/Is2projectApplication.java
+++ project/src/main/java/is2/Is2projectApplication.java
@@ -2,8 +2,10 @@ package is2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("domain")
 public class Is2projectApplication {
 
     public static void main(String[] args) {
