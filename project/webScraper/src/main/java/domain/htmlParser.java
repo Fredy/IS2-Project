f8a614d2621c5252b5556e84c7024fa651190349
Maria Cornejo Lupa <alecornejolupa2013@gmail.com>
2017-09-16 21:06:30 -0500

    #5 trying with the Tottus page

--- project/webScraper/src/main/java/domain/htmlParser.java
+++ project/webScraper/src/main/java/domain/htmlParser.java
@@ -8,7 +8,7 @@ public class htmlParser {
     Document document;
     try 
     {
-  	  document = Jsoup.connect("http://google.com.pe/").get();
+  	  document = Jsoup.connect("http://www.tottus.com.pe/tottus/browse/Electrohogar-Tecnolog%C3%ADa-Laptops/_/N-82nnyu").get();
   	  String title = document.title();
 	    System.out.println("Title: " + title);
     }
