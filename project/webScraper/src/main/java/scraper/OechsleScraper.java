3a81e2b791bec920b073d77e2e03a5dd7be029b1
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-09-23 17:42:34 -0500

    #6 functions for oechsle scraping finished

--- project/webScraper/src/main/java/scraper/OechsleScraper.java
+++ project/webScraper/src/main/java/scraper/OechsleScraper.java
@@ -13,39 +13,25 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 @Component
-public class oechsleScraper implements Scraper {
+public class OechsleScraper implements Scraper {
+
+  private String urlOechsle = "http://www.oechsle.pe/laptops";
 
   @Override
-  public List<Product> parseProduct() {
-    return null;
-  }
-
-  @Override
-  public void saveProduct(List<Product> product) {
-
-  }
-
-  @Override
-  public List<Product> getProduct() {
-    return null;
+  public List<Product> parseProducts() {
+    return this.getProductsData(this.urlOechsle);
   }
 
   @Override
   public Shop parseShop() {
-    return null;
+    Shop oechsleShop = new Shop();
+    oechsleShop.setName("Oechsle Peru");
+    oechsleShop.setAddress("Av. Ejército 1009 - CC Real Plaza Arequipa ");
+    oechsleShop.setUrl("http://www.oechsle.pe/");
+    return oechsleShop;
   }
 
-  @Override
-  public void saveShop(Shop shop) {
-
-  }
-
-  @Override
-  public Shop getShop() {
-    return null;
-  }
-
-  public Collection<String> getUrlsProducts(String baseUrl) {
+  private Collection<String> getUrlsProducts(String baseUrl) {
     /*
     * Returns a vector with all urls of products of @Param baseUrl
     */
@@ -63,25 +49,31 @@ public class oechsleScraper implements Scraper {
     return urls;
   }
 
-  public String getValueData(Document doc, String nameField) {
-    /* Returns text of a table.td with class ends in @Param nameField
-    * */
+  private String getValueData(Document doc, String nameField) {
+    /*
+    * Returns text of a table.td with class ends in @Param nameField
+     */
     String query = "td[class$=" + nameField + "]";
     Elements values = doc.select(query);
     if (values.isEmpty()) {
-      return "Not Found";
+      return "";
     }
     return values.text();
   }
 
-  String formatPrice(String priceText) {
+  private String getProductName(Document doc) {
+    Elements values = doc.body().select("div[class^='fn productName']");
+    return values.text();
+  }
+
+  private String formatPrice(String priceText) {
     String price = priceText.replaceAll(",", "");
     Integer position = priceText.indexOf(" ");
     price = price.substring(position + 1, price.length());
     return price;
   }
 
-  Vector<Double> getPrices(Document html) {
+  private Vector<Double> getPrices(Document html) {
     /*
     * Returns a vector of prices founded in @Param html
     * */
@@ -89,29 +81,30 @@ public class oechsleScraper implements Scraper {
     Vector<Double> prices = new Vector<Double>();
     String normal_price = html.select("strong.skuListPrice").text();
     String offer_price = html.select("strong.skuBestPrice").text();
-    //String web_price = html.select("div.newDiv").text(); //this is wrong
+
     normal_price = formatPrice(normal_price);
     offer_price = formatPrice(offer_price);
-    //web_price = formatPrice(web_price);
+
     if (Double.parseDouble(normal_price) == 0d) {
       normal_price = offer_price;
     }
     prices.add(Double.parseDouble(normal_price));
     prices.add(Double.parseDouble(offer_price));
-    //prices.add(Double.parseDouble(web_price));
+
     return prices;
   }
 
-  public Vector<Product> getData(String url) throws IOException {
-    /*
-      Return a vector of characteristcs of Product contents in @Param url
-    */
-    Vector<Product> products = new Vector<Product>();
-    Collection<String> urls = getUrlsProducts(url);
-    Vector<Double> prices = new Vector<Double>(3);
+  private Document getHtmlFromUrl(String url) throws IOException {
+    Document doc = Jsoup.connect(url).get();
+    return doc;
+  }
+
+  private Product getProduct(String urlProduct) {
     Product product = new Product();
-    for (String urlProduct : urls) {
-      Document doc = Jsoup.connect(urlProduct).get();
+    Vector<Double> prices = new Vector<Double>(2);
+    try {
+      Document doc = getHtmlFromUrl(urlProduct);
+      String nameProduct = getProductName(doc);
       String sku = getValueData(doc, "Sku");
       String model = getValueData(doc, "Modelo");
       String brand = getValueData(doc, "Marca");
@@ -119,13 +112,28 @@ public class oechsleScraper implements Scraper {
       prices = getPrices(doc);
       Double normal_price = prices.elementAt(0);
       Double offer_price = prices.elementAt(1);
-      //String web_price = prices.elementAt(2);
 
+      product.setSku(sku);
+      product.setName(nameProduct);
       product.setModel(model);
       product.setBrand(brand);
       product.setNormalPrice(normal_price);
       product.setOfferPrice(offer_price);
-      //product.setWebPrice(web_price);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return product;
+  }
+
+  private Vector<Product> getProductsData(String url) {
+    /*
+      Return a vector of characteristcs of Product contents in @Param url
+    */
+    Vector<Product> products = new Vector<Product>();
+    Collection<String> urls = getUrlsProducts(url);
+    Product product = new Product();
+    for (String urlProduct : urls) {
+      product = getProduct(urlProduct);
       products.addElement(product);
     }
 
