f770dc3c76f3b241f672fdc4392e3aae3fdab14b
Maria Cornejo <alecornejolupa2013@gmail.com>
2017-10-18 23:44:04 -0500

    #33 Implement getCategoryFunction

--- project/webScraper/src/main/java/crawler/TottusCrawler.java
+++ project/webScraper/src/main/java/crawler/TottusCrawler.java
@@ -13,7 +13,11 @@ import org.jsoup.select.Elements;
 
 public class TottusCrawler extends Crawler {
 
-  private String url = "http://www.tottus.com.pe/tottus/browse";
+  public TottusCrawler() {
+    this.url = "http://www.tottus.com.pe/tottus/";
+  }
+
+  private Document doc = null;
 
   private Document getHtmlFromURL(String PageURL) throws IOException {
     return Jsoup.connect(PageURL).userAgent("Mozilla").get();
@@ -21,44 +25,66 @@ public class TottusCrawler extends Crawler {
 
   private List<Category> getListCategories() {
     List<Category> res = new ArrayList<Category>();
-    try {
-      Document doc = this.getHtmlFromURL(url);
+    if (doc != null) {
       Elements npriceElements = doc.body()
           .getElementsByClass(
-              "icon icon-display");//bg-green white: categories with subcategories//menu-header-link:subcategoriesAll//mp-back//mp-level
+              "icon icon-display");
 
-      System.out.println("SIZE: " + npriceElements.size());
+      System.out.println("SIZE Categories: " + npriceElements.size());
       for (Element element : npriceElements) {
 
         String product = element.text();
-        System.out.println("product:" + product);
         String relUrl = element.getElementsByTag("a")
             .attr("abs:href");
-        System.out.println("rel: " + relUrl);
         Category tmp = new Category();
         tmp.setName(product);
         tmp.setUrl(relUrl);
         res.add(tmp);
       }
-
-
-    } catch (IOException e) {
-      e.printStackTrace();
     }
+
     return res;
   }
 
+  private List<SubCategory> getListSubCategories() {
+    List<SubCategory> res = new ArrayList<SubCategory>();
+    if (doc != null) {
+      Elements npriceElements = doc.body()
+          .getElementsByClass(
+              "menu-header-link");
+
+      System.out.println("SIZE subCategories: " + npriceElements.size());
+      for (Element element : npriceElements) {
+
+        String product = element.text();
+        String relUrl = element.getElementsByTag("a")
+            .attr("abs:href");
+        if (relUrl.contains("browse/")) {
+          SubCategory tmp = new SubCategory();
+          tmp.setName(product);
+          tmp.setUrl(relUrl);
+          res.add(tmp);
+        }
+      }
+    }
+
+    return res;
+  }
 
   @Override
   public List<Category> getCategories() {
-
+    try {
+      doc = this.getHtmlFromURL(url);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return getListCategories();
   }
 
 
   @Override
   public List<SubCategory> getSubCategories() {
-    return null;
+    return getListSubCategories();
   }
 
   @Override
