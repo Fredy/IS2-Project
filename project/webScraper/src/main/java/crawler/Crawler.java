d735071b7a1beff6f7fe5fde859c2f5371d3bf42
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-10-19 22:23:59 -0500

    #31 Improving Crawler abstract class

--- project/webScraper/src/main/java/crawler/Crawler.java
+++ project/webScraper/src/main/java/crawler/Crawler.java
@@ -4,16 +4,30 @@ import domain.Category;
 import domain.SubCategory;
 import domain.SubSubCategory;
 import java.util.List;
+import org.jsoup.nodes.Document;
 
 public abstract class Crawler {
 
   protected String url;
   // You should set the url value in implementation classes.
 
+  protected List<Category> categories;
+
+  /**
+   * This method builds the Categories Tree in class member categories.
+   * The intention of this method is to don't make multiple requests to the web page.
+   * @param homePage
+   * @return Builded tree in {@code List<Category>}
+   */
+  protected abstract List<Category> buildCategories(Document homePage);
+
+  /** Returns the builded tree of the current page**/
   public abstract List<Category> getCategories();
 
+  /** Returns all the subCategories of the current page **/
   public abstract List<SubCategory> getSubCategories();
 
+  /** Returns all the subSubCategories of the corrent page**/
   public abstract List<SubSubCategory> getSubSubCategories();
 
 }
