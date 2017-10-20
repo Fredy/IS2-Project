306b4a7cc74e11d7a55186607b83baa8a6a9669d
Maria Cornejo <alecornejolupa2013@gmail.com>
2017-10-19 23:21:45 -0500

    #33 GetListSucategories and GetSubcategoryBycategoryName implemented

--- project/webScraper/src/main/java/crawler/TottusCrawler.java
+++ project/webScraper/src/main/java/crawler/TottusCrawler.java
@@ -14,10 +14,17 @@ import org.jsoup.select.Elements;
 public class TottusCrawler extends Crawler {
 
   public TottusCrawler() {
+
     this.url = "http://www.tottus.com.pe/tottus/";
+    this.categories = null;
+    this.subCategories = null;
+    this.subSubCategories = null;
   }
 
   private Document doc = null;
+  private List<Category> categories;
+  private List<SubCategory> subCategories;
+  private List<SubSubCategory> subSubCategories;
 
   private Document getHtmlFromURL(String PageURL) throws IOException {
     return Jsoup.connect(PageURL).userAgent("Mozilla").get();
@@ -25,6 +32,11 @@ public class TottusCrawler extends Crawler {
 
   private List<Category> getListCategories() {
     List<Category> res = new ArrayList<Category>();
+    try {
+      doc = this.getHtmlFromURL(url);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     if (doc != null) {
       Elements npriceElements = doc.body()
           .getElementsByClass(
@@ -34,11 +46,16 @@ public class TottusCrawler extends Crawler {
       for (Element element : npriceElements) {
 
         String product = element.text();
+        //System.out.println("CATEGORIa A: "+ product);
+        product = product.replace(",", "");
+        product = product.replace(" ", "-");
+        //System.out.println("CATEGORIa B: "+ product);
         String relUrl = element.getElementsByTag("a")
             .attr("abs:href");
         Category tmp = new Category();
         tmp.setName(product);
         tmp.setUrl(relUrl);
+        tmp.setSubCategories(getListSubCategoriesByCategoryName(product));
         res.add(tmp);
       }
     }
@@ -46,8 +63,9 @@ public class TottusCrawler extends Crawler {
     return res;
   }
 
-  private List<SubCategory> getListSubCategories() {
+  private List<SubCategory> getListSubCategoriesByCategoryName(String categoryName) {
     List<SubCategory> res = new ArrayList<SubCategory>();
+
     if (doc != null) {
       Elements npriceElements = doc.body()
           .getElementsByClass(
@@ -57,9 +75,11 @@ public class TottusCrawler extends Crawler {
       for (Element element : npriceElements) {
 
         String product = element.text();
+
         String relUrl = element.getElementsByTag("a")
             .attr("abs:href");
-        if (relUrl.contains("browse/")) {
+
+        if (relUrl.contains("browse/" + categoryName)) {
           SubCategory tmp = new SubCategory();
           tmp.setName(product);
           tmp.setUrl(relUrl);
@@ -71,25 +91,58 @@ public class TottusCrawler extends Crawler {
     return res;
   }
 
+  private List<SubCategory> getListSubCategories() {
+    List<SubCategory> res = new ArrayList<SubCategory>();
+
+    if (doc != null) {
+      Elements npriceElements = doc.body()
+          .getElementsByClass(
+              "menu-header-link");//menu-header-link//bg-green white
+
+      System.out.println("SIZE subCategories: " + npriceElements.size());
+      for (Element element : npriceElements) {
+
+        String product = element.text();
+
+        String relUrl = element.getElementsByTag("a")
+            .attr("abs:href");
+
+        if (relUrl.contains("browse/")) {
+          //System.out.println("Sub:"+ product);
+          //System.out.println("hrefs:"+relUrl);
+          SubCategory tmp = new SubCategory();
+          tmp.setName(product);
+          tmp.setUrl(relUrl);
+          res.add(tmp);
+        }
+      }
+    }
+
+    return res;
+  }
+
+
   @Override
   public List<Category> getCategories() {
-    try {
-      doc = this.getHtmlFromURL(url);
-    } catch (IOException e) {
-      e.printStackTrace();
+    if (categories == null) {
+      categories = getListCategories();
     }
-    return getListCategories();
+    return this.categories;
   }
 
 
   @Override
   public List<SubCategory> getSubCategories() {
-    return getListSubCategories();
+    if (subCategories == null) {
+
+      subCategories = getListSubCategories();
+    }
+    return this.subCategories;
   }
 
   @Override
   public List<SubSubCategory> getSubSubCategories() {
-    return null;
+    return this.subSubCategories;
   }
 
 
