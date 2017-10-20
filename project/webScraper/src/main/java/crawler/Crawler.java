3e88c2486954fe8e6d2bdedfbe1d67b25ae9e6a6
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-10-20 16:30:44 -0500

    #31 Removed unnecesary methods

--- project/webScraper/src/main/java/crawler/Crawler.java
+++ project/webScraper/src/main/java/crawler/Crawler.java
@@ -1,10 +1,7 @@
 package crawler;
 
 import domain.Category;
-import domain.SubCategory;
-import domain.SubSubCategory;
 import java.util.List;
-import org.jsoup.nodes.Document;
 
 public abstract class Crawler {
 
@@ -14,20 +11,15 @@ public abstract class Crawler {
   protected List<Category> categories;
 
   /**
-   * This method builds the Categories Tree in class member categories.
-   * The intention of this method is to don't make multiple requests to the web page.
-   * @param homePage
+   * This method builds the Categories Tree in class member categories. The intention of this method
+   * is to don't make multiple requests to the web page.
+   *
    * @return Builded tree in {@code List<Category>}
    */
-  protected abstract List<Category> buildCategories(Document homePage);
+  protected abstract List<Category> buildCategories();
 
-  /** Returns the builded tree of the current page**/
-  public abstract List<Category> getCategories();
-
-  /** Returns all the subCategories of the current page **/
-  public abstract List<SubCategory> getSubCategories();
-
-  /** Returns all the subSubCategories of the corrent page**/
-  public abstract List<SubSubCategory> getSubSubCategories();
-
+  public List<Category> getCategories() {
+    this.categories = buildCategories();
+    return this.categories;
+  }
 }
