b3d330dd8a9992a183d7c3c9e4566bee7c3f5989
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-10-20 20:13:29 -0500

    #30 Adding documentation to some querys

--- project/webScraper/src/main/java/repository/ProductRepository.java
+++ project/webScraper/src/main/java/repository/ProductRepository.java
@@ -8,21 +8,41 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+  /**
+   * @return All Products with name {@Code brandName}
+   */
   @Query("SELECT p FROM Product p WHERE p.brand = ?1")
   Collection<Product> getProductsByBrand(String brandName);
 
+  /**
+   * @return All products with model {@Code modelName}
+   */
   @Query("SELECT p FROM Product p WHERE p.model = ?1")
   Collection<Product> getProductsByModel(String modelName);
 
+  /**
+   * @return All Products stored in date {@Code date}
+   */
   @Query("SELECT p FROM Product p WHERE p.date = ?1")
   Collection<Product> getProductsByDate(Date date);
 
+  /**
+   * @param first Lower date limit
+   * @param last Lpper date limit
+   * @return All products stored between {@code first} and {@code last} dates
+   */
   @Query(value = "SELECT p FROM Product p WHERE p.date BETWEEN ?1 AND ?2 ")
   Collection<Product> getProductsBetweenDates(Date first, Date last);
 
+  /**
+   * @return All Products before than date {@code date}
+   */
   @Query("SELECT p FROM Product p WHERE p.date <= ?1")
   Collection<Product> getProductsBeforeDate(Date date);
 
+  /**
+   * @return All Products after than date {@code date}
+   */
   @Query("SELECT p FROM Product p WHERE p.date >= ?1")
   Collection<Product> getProductsAfterDate(Date date);
 
