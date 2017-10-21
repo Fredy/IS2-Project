333dc876692aaa5997b77805f9a3a6dd0ce1469e
Alfred Chavez <alfredchavezurday@gmail.com>
2017-10-21 08:39:09 -0500

    #37 Formatted according Google Java Coding Style

--- project/webScraper/src/main/java/crawler/RipleyCrawler.java
+++ project/webScraper/src/main/java/crawler/RipleyCrawler.java
@@ -13,8 +13,12 @@ import org.jsoup.select.Elements;
 
 public class RipleyCrawler extends Crawler {
 
-  private Document getHtmlFromUrl(String PageUrl) throws IOException {
-    return Jsoup.connect(PageUrl).userAgent("Mozilla/5.0").get();
+  RipleyCrawler() {
+    this.setUrl("http://simple.ripley.com.pe");
+  }
+
+  private Document getHtmlFromUrl(String pageUrl) throws IOException {
+    return Jsoup.connect(pageUrl).userAgent("Mozilla/5.0").get();
   }
 
   private String getUrl() {
@@ -27,67 +31,68 @@ public class RipleyCrawler extends Crawler {
 
   @Override
   protected List<Category> buildCategories() {
-    this.setUrl("http://simple.ripley.com.pe");
-    ArrayList<Category> RCategories = new ArrayList<>();
-    Document Page;
+    ArrayList<Category> rCategories = new ArrayList<>();
+    Document page;
     try {
-      Page = getHtmlFromUrl(url);
+      page = getHtmlFromUrl(url);
     } catch (IOException e) {
       e.printStackTrace();
-      return RCategories;
+      return rCategories;
     }
-    Elements elements = Page.getElementsByClass("main-categories")
+    Elements elements = page.getElementsByClass("main-categories")
         .select("[class^=main-category first-column]");
     for (Element element : elements) {
-      String Name = element.select("[class^=main-category-name]")
+      String name = element.select("[class^=main-category-name]")
           .select("[class^=main-category-active-text]").text();
-      Name = Name.toLowerCase();
+      name = name.toLowerCase();
       Category category = new Category();
-      category.setName(Name);
+      category.setName(name);
       category.setUrl(element.select("[class^=main-category-name]").attr("abs:href"));
       buildSubCategories(element, category);
-      RCategories.add(category);
+      rCategories.add(category);
     }
-    return RCategories;
+    return rCategories;
   }
 
-  private ArrayList<SubCategory> buildSubCategories(Element Category, Category category) {
-    ArrayList<SubCategory> RSubCategories = new ArrayList<>();
-    Elements elements = Category.select("[class^=child-categories second-column]")
+  private ArrayList<SubCategory> buildSubCategories(Element categoryEl, Category category) {
+    ArrayList<SubCategory> rSubCategories = new ArrayList<>();
+    Elements elements = categoryEl.select("[class^=child-categories second-column]")
         .select("[class=child-category]");
     for (Element element : elements) {
       Elements realCategory = element.select("[href~=^/[\\-a-z]+/[\\-a-z]+$]");
       if (!realCategory.isEmpty()) {
-        String Name = realCategory.first().text();
-        Name = Name.toLowerCase();
-        if(Name.equals("marcas"))continue;
+        String name = realCategory.first().text();
+        name = name.toLowerCase();
+        if (name.equals("marcas")) {
+          continue;
+        }
         //System.out.println("\t" + Name);
         SubCategory subCategory = new SubCategory();
-        subCategory.setName(Name);
+        subCategory.setName(name);
         subCategory.setUrl(realCategory.attr("abs:href"));
         buildSubSubCategories(element, subCategory);
-        RSubCategories.add(subCategory);
+        rSubCategories.add(subCategory);
       }
     }
-    category.setSubCategories(RSubCategories);
-    return RSubCategories;
+    category.setSubCategories(rSubCategories);
+    return rSubCategories;
   }
 
-  private ArrayList<SubSubCategory> buildSubSubCategories(Element SubCategory,
+  private ArrayList<SubSubCategory> buildSubSubCategories(Element subCategoryEl,
       SubCategory subCategory) {
-    ArrayList<SubSubCategory> RSubSubCategories = new ArrayList<>();
-    Elements elements = SubCategory.select("[href~=^/[\\-a-z]+/[\\-a-z]+/[\\-a-z]+$]");
+    ArrayList<SubSubCategory> rSubSubCategories = new ArrayList<>();
+    Elements elements = subCategoryEl.select("[href~=^/[\\-a-z]+/[\\-a-z]+/[\\-a-z]+$]");
     for (Element element : elements) {
-      String Name = element.text();
-      Name = Name.toLowerCase();
+      String name = element.text();
+      name = name.toLowerCase();
       //System.out.println("\t\t"+Name);
       SubSubCategory subSubCategory = new SubSubCategory();
-      subSubCategory.setName(Name);
+      subSubCategory.setName(name);
       subSubCategory.setUrl(element.attr("abs:href"));
-      RSubSubCategories.add(subSubCategory);
+      rSubSubCategories.add(subSubCategory);
     }
-    subCategory.setSubSubCategories(RSubSubCategories);
-    return RSubSubCategories;
+    subCategory.setSubSubCategories(rSubSubCategories);
+    return rSubSubCategories;
   }
 
 }
