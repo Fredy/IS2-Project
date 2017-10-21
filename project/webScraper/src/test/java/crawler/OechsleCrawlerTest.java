fe2966da949841535c3bc48132a5cda8ebe229cd
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-10-21 01:00:54 -0500

    #36 create Oechsle Crawler test

---
+++ project/webScraper/src/test/java/crawler/OechsleCrawlerTest.java
@@ -0,0 +1,14 @@
+package crawler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OechsleCrawlerTest {
+
+  @Test
+  public void buildCategories() throws Exception {
+
+  }
+
+}
