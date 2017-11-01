a295a33bdbf8367f7327d098766ae6eb85e15845
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-11-01 12:36:19 -0500

    #69 Linio Crawler: code format

--- project/webScraper/src/main/java/crawler/LinioCrawler.java
+++ project/webScraper/src/main/java/crawler/LinioCrawler.java
@@ -127,7 +127,6 @@ public class LinioCrawler extends Crawler {
         subCategory.setUrl(url);
         subCategories.add(subCategory);
 
-
         logger.debug("-Subcategory: " + name + " : " + url);
       } else {
         SubSubCategory tmpSSC = new SubSubCategory();
