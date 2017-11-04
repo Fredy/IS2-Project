d314913f35c303a403715df94c641841faf04393
Alfred Chavez <alfredchavezurday@gmail.com>
2017-11-04 13:15:39 -0500

    #84 Created Ripley Scraper Test File

---
+++ project/webScraper/src/test/java/scraper/RipleyScraperTest.java
@@ -0,0 +1,17 @@
+package scraper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RipleyScraperTest {
+
+  @Test
+  public void parseProducts() throws Exception {
+  }
+
+  @Test
+  public void parseShop() throws Exception {
+  }
+
+}
