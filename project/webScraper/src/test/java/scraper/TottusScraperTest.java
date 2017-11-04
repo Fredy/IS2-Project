3e580fa5d2059c94f67d8cab4adb48c3488bdc02
Maria Cornejo <alecornejolupa2013@gmail.com>
2017-11-04 11:04:21 -0500

    #80 add Skeleton Tottus Scraper Tests

---
+++ project/webScraper/src/test/java/scraper/TottusScraperTest.java
@@ -0,0 +1,34 @@
+package scraper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TottusScraperTest {
+
+  @Test
+  public void urlToJsonArray() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void oneToVector() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void vectorStringsToProducts() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void getShopData() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void parseProducts() throws Exception {
+    assertTrue(true);
+  }
+
+}
