fbe7e780e8f9ded90f45c0bb00eb28a41fc704ba
Maria Cornejo <alecornejolupa2013@gmail.com>
2017-11-04 11:04:43 -0500

    #81 add Skeleton Tottus Crawler Tests

---
+++ project/webScraper/src/test/java/crawler/TottusCrawlerTest.java
@@ -0,0 +1,28 @@
+package crawler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TottusCrawlerTest {
+
+  @Test
+  public void buildCategories() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void crawlingCategory() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void crawlingSubCategory() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void crawlingSubSubCategory() throws Exception {
+    assertTrue(true);
+  }
+}
