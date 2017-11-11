f0d2b119ea7fe0a4e6cf21ba0f05298939fc3da5
Maria Cornejo <alecornejolupa2013@gmail.com>
2017-11-10 20:35:46 -0500

    #80 Fix url falseReference to TottusPage

--- project/webScraper/src/test/java/scraper/TottusScraperTest.java
+++ project/webScraper/src/test/java/scraper/TottusScraperTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.assertTrue;
 
 import domain.Product;
 import domain.SubSubCategory;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 public class TottusScraperTest {
@@ -15,26 +18,29 @@ public class TottusScraperTest {
   @Test
   public void urlToJsonArray() throws Exception {
     TottusScraper tottusScraper = new TottusScraper();
-    String jSonArrayScraped = tottusScraper.urlToJsonArray(
-        "http://www.tottus.com.pe/tottus/browse/Abarrotes-y-Despensa-Arroz-Arroz-Integral/_/N-7nn7of");
+    File htmlFile = new File("src/test/resources/scraper/tottusSubSubCategoryPage.html");
+    Document document = Jsoup.parse(htmlFile, "UTF-8", "http://www.tottus.com.pe/tottus/");
+
+    String jSonArrayScraped = tottusScraper.urlToJsonArray(document);
     String comparisonJsonArray =
         "{\"id\":\"40879173\",\"name\":\"Arroz Extra Integral\",\"brand\":\"TOTTUS\",\"price\":\"21.15\",\"position\":0,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"},"
-      + "{\"id\":\"40737788\",\"name\":\"Arroz Extra Integral\",\"brand\":\"TOTTUS\",\"price\":\"3.25\",\"position\":1,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"},"
-      + "{\"id\":\"40360383\",\"name\":\"Arroz Integral \",\"brand\":\"COSTENO\",\"price\":\"3.35\",\"position\":2,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}";
+            + "{\"id\":\"40360383\",\"name\":\"Arroz Integral \",\"brand\":\"COSTENO\",\"price\":\"3.35\",\"position\":1,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}";
     assertEquals(comparisonJsonArray, jSonArrayScraped);
   }
 
   @Test
   public void oneToVector() throws Exception {
     TottusScraper tottusScraper = new TottusScraper();
-    String listOfProducts = tottusScraper.urlToJsonArray(
-        "http://www.tottus.com.pe/tottus/browse/Abarrotes-y-Despensa-Arroz-Arroz-Integral/_/N-7nn7of");
+    File htmlFile = new File("src/test/resources/scraper/tottusSubSubCategoryPage.html");
+    Document document = Jsoup.parse(htmlFile, "UTF-8", "http://www.tottus.com.pe/tottus/");
+
+    String listOfProducts = tottusScraper.urlToJsonArray(document);
+
     List<String> dataScraped = tottusScraper.oneToVector(listOfProducts);
     List<String> dataExpected = new ArrayList<>();
     String expected[] = {
         "{\"id\":\"40879173\",\"name\":\"Arroz Extra Integral\",\"brand\":\"TOTTUS\",\"price\":\"21.15\",\"position\":0,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}",
-        "{\"id\":\"40737788\",\"name\":\"Arroz Extra Integral\",\"brand\":\"TOTTUS\",\"price\":\"3.25\",\"position\":1,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}",
-        "{\"id\":\"40360383\",\"name\":\"Arroz Integral \",\"brand\":\"COSTENO\",\"price\":\"3.35\",\"position\":2,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}"};
+        "{\"id\":\"40360383\",\"name\":\"Arroz Integral \",\"brand\":\"COSTENO\",\"price\":\"3.35\",\"position\":1,\"category\":\"Abarrotes y Despensa/Arroz/Arroz Integral\",\"list\":\"plp_categoria_Arroz_Arroz Integral\"}"};
     Collections.addAll(dataExpected, expected);
 
     assertEquals(dataExpected, dataScraped);
@@ -43,35 +49,36 @@ public class TottusScraperTest {
   @Test
   public void vectorStringsToProducts() throws Exception {
     TottusScraper tottusScraper = new TottusScraper();
-    String listOfProducts = tottusScraper.urlToJsonArray(
-        "http://www.tottus.com.pe/tottus/browse/Abarrotes-y-Despensa-Arroz-Arroz-Integral/_/N-7nn7of");
+    File htmlFile = new File("src/test/resources/scraper/tottusSubSubCategoryPage.html");
+    Document document = Jsoup.parse(htmlFile, "UTF-8", "http://www.tottus.com.pe/tottus/");
+
+    String listOfProducts = tottusScraper.urlToJsonArray(document);
     List<String> dataScraped = tottusScraper.oneToVector(listOfProducts);
-    List<Product> productScraped = tottusScraper.vectorStringsToProducts(dataScraped,
-        "http://www.tottus.com.pe/tottus/browse/Abarrotes-y-Despensa-Arroz-Arroz-Integral/_/N-7nn7of");
-    String expectedname = "Arroz Extra Integral";
-    Double expectedWebPrice = 3.25;
-    String expectedSku = "40737788";
-    String expectedBrand = "TOTTUS";
+    List<Product> productScraped = tottusScraper.vectorStringsToProducts(dataScraped, document);
+    String expectedname = "Arroz Integral ";
+    Double expectedWebPrice = 3.35;
+    String expectedSku = "40360383";
+    String expectedBrand = "COSTENO";
 
-    assertTrue(productScraped.get(1).getBrand().contentEquals(expectedBrand));
-    assertTrue(productScraped.get(1).getName().contentEquals(expectedname));
-    assertTrue(productScraped.get(1).getSku().contentEquals(expectedSku));
-    assertTrue(productScraped.get(1).getWebPrice().equals(expectedWebPrice));
-
+    assertEquals(productScraped.get(1).getBrand(), expectedBrand);
+    assertEquals(productScraped.get(1).getName(), expectedname);
+    assertEquals(productScraped.get(1).getSku(), expectedSku);
+    assertEquals(productScraped.get(1).getWebPrice(), expectedWebPrice);
   }
 
   @Test
   public void parseProducts() throws Exception {
     TottusScraper tottusScraper = new TottusScraper();
+
     SubSubCategory crawledSubSubCategory = new SubSubCategory();
     crawledSubSubCategory.setUrl(
         "http://www.tottus.com.pe/tottus/browse/Abarrotes-y-Despensa-Arroz-Arroz-Integral/_/N-7nn7of");
     List<Product> crawledProducts = tottusScraper.parseProducts(crawledSubSubCategory);
 
-    String expectedname = "Arroz Extra Integral";
-    Double expectedWebPrice = 3.25;
-    String expectedSku = "40737788";
-    String expectedBrand = "TOTTUS";
+    String expectedname = "Arroz Integral ";
+    Double expectedWebPrice = 3.35;
+    String expectedSku = "40360383";
+    String expectedBrand = "COSTENO";
 
     assertTrue(crawledProducts.get(1).getBrand().contentEquals(expectedBrand));
     assertTrue(crawledProducts.get(1).getName().contentEquals(expectedname));
